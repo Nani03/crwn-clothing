d1afe1d1a5137fa9266c9b5060d1e7c4af499e03
nani03 <leela.rroyal@gmail.com>
2021-04-09 16:44:21 +0530

    created prject and committed files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+import Homepage from "./homepage.component";
+function App() {
+  return (
+    <div>
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
