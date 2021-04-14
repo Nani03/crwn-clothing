a3968144e9b41a95a6acfe729157b6c4504d92d2
nani03 <leela.rroyal@gmail.com>
2021-04-14 18:47:42 +0530

    added sign in and button components to sign in page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { Route, Switch } from "react-router-dom";
 import Homepage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+
 
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
       <switch>
         <Route exact path="/" component={Homepage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUpPage} />
       </switch>
     </div>
   );
